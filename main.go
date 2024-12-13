7628d73dbdac35efffc6cd41c8f9767c1bf13083
Syed Azeez <syedazeez337@gmail.com>
2024-12-13 17:51:46 +0530

    gin init

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	gin.SetMode(gin.ReleaseMode)
+
+	fmt.Println("Server is starting...")
+
+	r := gin.Default()
+
+    r.POST()
+
+	r.GET("/hello", hello)
+	r.GET("/ping", pong)
+
+	r.Run()
+
+}
+
+type HeaderGin gin.H
+
+var helloMsg = HeaderGin{
+	"message": "Hello world",
+}
+
+var pongMsg = HeaderGin{
+	"message": "pong",
+}
+
+func hello(c *gin.Context) {
+	c.JSON(http.StatusOK, helloMsg)
+}
+
+func pong(c *gin.Context) {
+	c.JSON(http.StatusOK, pongMsg)
+}
